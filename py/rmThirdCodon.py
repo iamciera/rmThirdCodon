eeb0184325b697f99c8cf53bd31d7aec64b7aedc
Ciera Martinez <ciera.martinez@gmail.com>
2015-01-29 15:59:24 +0900

    have the message system working, but the script is still off by 14 nucleotides.

--- py/rmThirdCodon.py
+++ py/rmThirdCodon.py
@@ -8,8 +8,6 @@
 
 #The verifying part needs a bit of work, but you should get 1/3 of the 
 #nucleotides missing from the file that was input.
-#- How do I print to console or file?  Right now the verifying part prints to 
-#only the out put file
 # - The result is 14 nucleotides off!
 # - Write a true false test to test it. 
 
@@ -23,22 +21,36 @@ sys.stdout = data
 
 fastaFile = open(sys.argv[1]) #file that contains fasta file
 
-fastaRead = fastaFile.read() #Makes a one item string
+fastaRead = fastaFile.read() 
 
-#this removes the third codon and reasults in one item string
+#this removes the third codon
 thirdGone = re.sub(r"([ATCG])([ATCG])[ATCG]", r"\1\2", fastaRead)
 print thirdGone
 
-#Verify
-beforeMatchResult = re.findall(r"[ATCG][ATCG][ATCG]*",fastaRead)
-beforeResult = ''.join(beforeMatchResult)
-lengthBefore = len(beforeResult)
-print 'before %d' %(lengthBefore)
+#resets standard output back to terminal
+sys.stdout = sys.__stdout__
 
-afterMatchResult = re.findall(r"[ATCG][ATCG][ATCG]*",thirdGone)
-afterResult = ''.join(afterMatchResult)
+#Verification Test
+
+#How many nucleotides before the third codon was removed?
+nucBefore = re.findall(r"[ATCG][ATCG][ATCG]*",fastaRead)
+beforeResult = ''.join(nucBefore)
+lengthBefore = len(beforeResult)
+print '\nbefore %d' %(lengthBefore)
+
+#How many nucleotides after the third codon was removed?
+nucAfter = re.findall(r"[ATCG][ATCG][ATCG]*",thirdGone)
+afterResult = ''.join(nucAfter)
 lengthAfter = len(afterResult)
 print 'after %d' %(lengthAfter)
 
+#Did it work?  The message displayed.
+if (lengthBefore/3)*2 != lengthAfter:
+	print '\nWARNING: Something is off! You should have %d nucleotides, but you have %d\n' %((lengthBefore/3)*2, lengthAfter);
+
+if (lengthBefore/3)*2 == lengthAfter:
+	print '\nLooks Good!\n'
+		
 fastaFile.close()
 data.close()
+
