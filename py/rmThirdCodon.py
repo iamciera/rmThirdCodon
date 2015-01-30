6c42bd011682afc4aa5ccda095fbfa7f54cc417a
Ciera Martinez <ciera.martinez@gmail.com>
2015-01-30 18:20:37 +0900

    have the script working to remove third codon per sequence.  I still need to update the tests.

--- py/rmThirdCodon.py
+++ py/rmThirdCodon.py
@@ -9,48 +9,72 @@
 #The verifying part needs a bit of work, but you should get 1/3 of the 
 #nucleotides missing from the file that was input.
 # - The result is 14 nucleotides off!
-# - Write a true false test to test it. 
+
+
+#We should really start the counting fresh for each sequence, 
+#that way we make sure that we are geting the third codon for each sequence 
+#and one sequence doesn't mess up the rest. IN order to that, seperate each 
+#sequence into it's own item in a list. 
+#also, instead of getting rid of the nucleotide, maybe we just add a "-" that 
+#the alignment is not messed up. 
+
+#To-do
+
+# [ ] Split each sequence into an item in a list
+# [ ] How many nucleotides in each sequence. 
+# [ ] Can they be divided by three?
 
 import re
 import sys	
 
 #This sets up output file
 orig_stdout = sys.stdout
-data = open("./fasta/rmThirdCodonOutput.fasta", 'w')
-sys.stdout = data
+# data = open("./fasta/rmThirdCodonOutput.fasta", 'w')
+# sys.stdout = data
 
 fastaFile = open(sys.argv[1]) #file that contains fasta file
 
 fastaRead = fastaFile.read() 
 
+
+#Split by > 
+fastaList = fastaRead.split(">")
+
+thirdGoneList = {}
 #this removes the third codon
-thirdGone = re.sub(r"([ATCG])([ATCG])[ATCG]", r"\1\2", fastaRead)
-print thirdGone
+for sequence in fastaList:
+	thirdGoneList[sequence] = re.sub(r"([ATCG])([ATCG])[ATCG]", r"\1\2-", sequence);
 
-#resets standard output back to terminal
-sys.stdout = sys.__stdout__
+thirdGoneString = ''.join(thirdGoneList)
+print thirdGoneString
 
-#Verification Test
+# #resets standard output back to terminal
+# sys.stdout = sys.__stdout__
 
-#How many nucleotides before the third codon was removed?
-nucBefore = re.findall(r"[ATCG][ATCG][ATCG]*",fastaRead)
-beforeResult = ''.join(nucBefore)
-lengthBefore = len(beforeResult)
-print '\nbefore %d' %(lengthBefore)
 
-#How many nucleotides after the third codon was removed?
-nucAfter = re.findall(r"[ATCG][ATCG][ATCG]*",thirdGone)
-afterResult = ''.join(nucAfter)
-lengthAfter = len(afterResult)
-print 'after %d' %(lengthAfter)
+# #Verification Test
+# #How many nucleotides before the third codon was removed?
+# nucBefore = re.findall(r"[ATCG][ATCG][ATCG]*",fastaRead)
+# beforeResult = ''.join(nucBefore)
+# lengthBefore = len(beforeResult)
+# print '\nbefore %d' %(lengthBefore)
 
-#Did it work?  The message displayed.
-if (lengthBefore/3)*2 != lengthAfter:
-	print '\nWARNING: Something is off! You should have %d nucleotides, but you have %d\n' %((lengthBefore/3)*2, lengthAfter);
+# lengths = [(len(i)/float(3)) for i in nucBefore]
+# print lengths
 
-if (lengthBefore/3)*2 == lengthAfter:
-	print '\nLooks Good!\n'
+# #How many nucleotides after the thrid codon was removed?
+# nucAfter = re.findall(r"[ATCG][ATCG][ATCG]*",thirdGone)
+# afterResult = ''.join(nucAfter)
+# lengthAfter = len(afterResult)
+# print 'after %d' %(lengthAfter)
+
+# #Did it work?  The message displayed.
+# if (lengthBefore/3)*2 != lengthAfter:
+# 	print '\nWARNING: Something is off! You should have %d nucleotides, but you have %d\n' %((lengthBefore/3)*2, lengthAfter);
+
+# if (lengthBefore/3)*2 == lengthAfter:
+# 	print '\nLooks Good!\n'
 		
-fastaFile.close()
-data.close()
+# fastaFile.close()
+# data.close()
 
